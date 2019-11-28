36d20bcdc5a63baf009a17b807c70b027396a53f
Jan <jan@optigo.net>
2019-11-27 18:45:34 -0800

    Implement 2 RPDU types
    Implemented the following:
    - Who-Is-Router-To-Network
    - I-Am-Router-To-Network

--- src/nsdu/rpdu.rs
+++ src/nsdu/rpdu.rs
@@ -3,6 +3,30 @@ use arrayref::array_ref;
 
 pub type DNET = u16;
 
+pub struct DNETs<'a> {
+    bytes: &'a [u8],
+}
+
+impl Iterator for DNETs<'_> {
+    type Item = DNET;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.bytes.len() {
+            0..=1 => None,
+            _ => {
+                let dnet = u16::from_be_bytes(*array_ref!(self.bytes, 0, 2));
+                self.bytes = &self.bytes[2..];
+                Some(dnet)
+            }
+        }
+    }
+}
+
+impl<'a> From<&'a [u8]> for DNETs<'a> {
+    fn from(bytes: &'a [u8]) -> Self {
+        Self { bytes }
+    }
+}
+
 fn try_parse_dnet(b: &[u8]) -> Result<DNET, Error> {
     if b.len() < 2 {
         Err(Error::Length("insufficient size for DNET"))
@@ -15,11 +39,13 @@ pub fn parse_rpdu(bytes: &[u8]) -> Result<RPDU, Error> {
     if bytes.is_empty() {
         return Err(Error::Length("no rpdu data"));
     }
-    // TODO: figure out if this is a single u16 or needs to be an iterator
     Ok(match bytes[0] {
-        0x00 => RPDU::WhoIsRouterToNetwork(try_parse_dnet(bytes)?),
-        0x01 => RPDU::IAmRouterToNetwork(try_parse_dnet(bytes)?),
-        0x02 => RPDU::ICouldBeRouterToNetwork(try_parse_dnet(bytes)?),
+        0x00 => RPDU::WhoIsRouterToNetwork(match try_parse_dnet(bytes) {
+            Ok(dnet) => Some(dnet),
+            Err(_) => None,
+        }),
+        0x01 => RPDU::IAmRouterToNetwork(bytes.into()),
+        0x02 => RPDU::ICouldBeRouterToNetwork(try_parse_dnet(bytes)?), // TODO: need to verify this one
         0x03 => RPDU::RejectMessageToNetwork,
         0x04 => RPDU::RouterBusyToNetwork,
         0x05 => RPDU::RouterAvailableToNetwork,
@@ -40,9 +66,9 @@ pub fn parse_rpdu(bytes: &[u8]) -> Result<RPDU, Error> {
     })
 }
 
-pub enum RPDU {
-    WhoIsRouterToNetwork(DNET),
-    IAmRouterToNetwork(DNET),
+pub enum RPDU<'a> {
+    WhoIsRouterToNetwork(Option<DNET>),
+    IAmRouterToNetwork(DNETs<'a>),
     ICouldBeRouterToNetwork(DNET),
     RejectMessageToNetwork,
     RouterBusyToNetwork,
